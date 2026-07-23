b19b8647f6db60f1d7c098dd49b2c6b004b7387e
Arnav Kirti <arnav.tkd@gmail.com>
2026-07-23 11:32:17 +0530

    updated vite config and fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/data/**/*', // Source folder
+          dest: 'data',            // Destination folder
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src', // Alias for the `src` directory
+    },
   },
+  assetsInclude: ['**/*.JPG'], // Add this line to handle .JPG files
+  server: {
+    port: 3000, // Specify the port you want to use
+  },
+  base: '/', // Set the base path for your app when deployed
 });
